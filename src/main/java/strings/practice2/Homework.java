69a8d1fb431b2a90bd535947494f8d23d00bafe1
vitaliynezhinskiy <SeekerOfConfessor@gmail.com>
2020-05-21 01:01:16 +0300

    little fix

--- src/main/java/strings/practice2/Homework.java
+++ src/main/java/strings/practice2/Homework.java
@@ -3,5 +3,6 @@ package strings.practice2;
 public class Homework {
     public static void main(String[] args) {
         System.out.println("Privet");
+        System.out.println("Privet2");
     }
 }
